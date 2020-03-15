fcbe9a0c7dc0093291b8f90a934c17dceebddd6c
Niklas Haim <55130904+Haim-hub@users.noreply.github.com>
2020-03-15 17:16:38 +0100

    Unity
    I Have made a basic room generator.
    
    The player can move.
    
    The camera move to each room.

---
+++ notGDCJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int speed = 5;
+    public Rigidbody2D rb;
+    public GameObject Cam;
+    Vector2 movement;
+
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        movement = Vector2.zero;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("RoomCol")){
+            Cam.transform.position = new Vector3(other.transform.position.x, other.transform.position.y, Cam.transform.position.z);
+            
+            
+            if(gameObject.transform.position.x < other.transform.position.x && Math.Abs(gameObject.transform.position.y - other.transform.position.y)<2){
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1.5f , other.transform.position.y ,gameObject.transform.position.z);
+            } else if(gameObject.transform.position.x > other.transform.position.x &&  Math.Abs(gameObject.transform.position.y - other.transform.position.y)<2){
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x - 1.5f , other.transform.position.y ,gameObject.transform.position.z);
+            } else if(gameObject.transform.position.y < other.transform.position.y){
+                gameObject.transform.position = new Vector3(other.transform.position.x, gameObject.transform.position.y + 1.5f ,gameObject.transform.position.z);
+            } else if(gameObject.transform.position.y > other.transform.position.y){
+                gameObject.transform.position = new Vector3(other.transform.position.x , gameObject.transform.position.y - 1.5f ,gameObject.transform.position.z);
+            }
+            
+        }
+        
+    }
+}
---
+++ notGDCJam/Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour{
+
+    public int openingDirection;
+    // 1 --> need bottom door
+    // 2 --> need top door
+    // 3 --> need left door
+    // 4 --> need right door
+    public GameObject Cam;
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+    void Start(){
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+
+    void Spawn(){
+        if(spawned == false){
+            switch(openingDirection){
+            case 1:
+                //Needs a room with a bottom door
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+                break;
+            case 2:
+                //Needs a room with a top door
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+                break;
+            case 3:
+                //Needs a room with a left door
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+                break;
+            case 4:
+                //Needs a room with a right door
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+                break;
+            }
+            spawned = true;
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("SpawnPoint")){
+            Destroy(gameObject);
+        }
+        if(other.CompareTag("RoomCol")){
+
+        }
+        
+    }
+
+
+
+}
